acd980d61ef522bc162f5a4ccf1e2d0efebf6fbb
hyeonjin6767 <parkhj067670@gmail.com>
2025-08-29 09:59:44 +0900

    Initial Commit

---
+++ PersonalPracticeProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonalPracticeProject
+//
+//  Created by 박현진 on 8/29/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
